363e195f26cfb97a9c5d01e71e1e0fc8fa24fddd
kpuk4o <kpuk4o@gmail.com>
2022-02-13 02:39:59 -0600

    Completed Chapter 5 exercises

--- src/exercises/Chapter4/school/Course.java
+++ src/exercises/Chapter4/school/Course.java
@@ -10,4 +10,22 @@ public class Course {
     private ArrayList<Student> students;
     //how many credits do you get
     private int numberOfCredits;
+
+    //Constructors
+    public Course(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " is worth " + this.numberOfCredits + "number of Credits";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Course) == false) return false;
+        Course myCouirseobj = (Course) obj;
+        if (myCouirseobj.name.equals(this.name)) return true;
+        else return false;
+    }
 }
--- src/exercises/Chapter4/school/Student.java
+++ src/exercises/Chapter4/school/Student.java
@@ -3,8 +3,8 @@ package exercises.Chapter4.school;
 public class Student {
 
     public static void main(String[] args) {
-        Student myself = new Student("Dimitar", 1, 1, 4.0);
-
+        Student myself = new Student("Dimitar", 30, 1, 4.0);
+        String myGradeLevel = myself.getGradeLevel();
     }
 
     private static int nextStudentId = 1;
@@ -31,6 +31,55 @@ public class Student {
 
     public String studentInfo() {
         return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        // Update the appropriate fields: numberOfCredits, gpa
+        //quality score = letter grade * number of credits
+        //total quality score
+        double currentTotalQualityScore = this.gpa * this.numberOfCredits;
+
+        double newQualityScore = grade * courseCredits;
+
+        double newTotalQualityScore = currentTotalQualityScore + newQualityScore;
+
+        int newTotalNumberOfCredits = this.numberOfCredits + courseCredits;
+        this.numberOfCredits = newTotalNumberOfCredits;
+
+        double newGpa = newQualityScore / newTotalNumberOfCredits;
+        this.gpa = newGpa;
+
+    }
+
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        // Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits)
+
+        if (this.numberOfCredits >= 0 && this.numberOfCredits <=29) {
+            return "Freshman";
+        } else if (this.numberOfCredits >= 30 && this.numberOfCredits <=59) {
+            return "Sophomore";
+        } else if (this.numberOfCredits >= 60 && this.numberOfCredits <=89) {
+            return "Junior";
+        } else if (this.numberOfCredits >= 90) {
+            return "Senior";
+        } else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.gpa;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Student) == false) return false;
+        Student myStundentobj = (Student) obj;
+        if (myStundentobj.name.equals(this.name)) return true;
+        else return false;
     }
 
     /* getters and setters omitted */
