5e38cdbe78f7357fcf45456a7dcf5ea026dd2bb8
kpuk4o <kpuk4o@gmail.com>
2022-02-13 01:33:45 -0600

    Completed Chapter 3 exercises

---
+++ src/exercises/Chapter4/school/Course.java
@@ -0,0 +1,13 @@
+package exercises.Chapter4.school;
+
+import java.util.ArrayList;
+
+public class Course {
+    //at least 3 fields
+    //name
+    private String name;
+    //student list
+    private ArrayList<Student> students;
+    //how many credits do you get
+    private int numberOfCredits;
+}
---
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn7interfaces;
+
+public class FlavorComparator implements Comprator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
